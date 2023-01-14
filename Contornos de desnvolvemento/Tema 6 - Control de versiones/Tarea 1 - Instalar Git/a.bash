0ff1f1ed5f2a07f2bd48e63ad7105cabdbdb085d
Rodrigo Iglesias <crampyy@gmail.com>
2023-01-14 14:44:58 +0100

    tarea contornos

---
+++ Contornos de desnvolvemento/Tema 6 - Control de versiones/Tarea 1 - Instalar Git/a.bash
@@ -0,0 +1,7 @@
+git config --global user.name "Rodrigo Iglesias"
+
+git config --global user.email "iglesias.nieto.rodrigo.ald@gmail.com"
+
+git config --global diff.tool "vscode"
+
+git config --list
