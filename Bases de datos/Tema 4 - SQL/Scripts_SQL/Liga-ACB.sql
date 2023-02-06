fd9e9616558892ac1f1d093c94ce71e593935547
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-02-06 21:44:56 +0100

    Inicio queries

--- Bases de datos/Tema 4 - SQL/Scripts_SQL/Liga-ACB.sql
+++ Bases de datos/Tema 4 - SQL/Scripts_SQL/Liga-ACB.sql
@@ -1,54 +1,98 @@
-drop database if exists LigaACB;
-create database LigaACB;
-use LigaACB;
-
-drop table if exists EQUIPOS;
-create table EQUIPOS(
-	idEquipo int unsigned auto_increment primary key,
-    nombreEquipo varchar(30) not null,
-    ciudad varchar(30) not null,
-    webOficial varchar(120) not null,
-    puntos int unsigned not null
-);
-
-drop table if exists JUGADORES;
-create table JUGADORES(
-	idJugador int unsigned auto_increment primary key,
-    nombre varchar(30) not null,
-    apellido varchar(60) not null,
-    puesto set('Escolta','Alero','Pivot','Base') not null,
-    idCapitan int unsigned not null,
-    fechaAlta DATE not null,
-    salario double(7,2) not null,
-    idEquipo int unsigned not null,
-    altura double(2,2),
-    foreign key (idCapitan) references JUGADORES(idJugador)
-    on update cascade on delete cascade,
-    foreign key (idEquipo) references EQUIPOS(idEquipo)
-    on update cascade on delete cascade
-);
+DROP DATABASE IF EXISTS `ligaACB`;
+CREATE SCHEMA `ligaACB`;
+USE `ligaACB` ;
 
 
-drop table if exists PARTIDOS;
-create table PARTIDOS(
-	eLocal int unsigned not null,
-    eVisitante int unsigned not null,
-    resultado varchar (7) not null,
-    fecha date not null,
-    arbitro int unsigned not null,
-    primary key(eLocal, eVisitante),
-    foreign key (eLocal) references EQUIPOS(idEquipo) 
-    on update cascade on delete cascade,
-    foreign key (eVisitante) references EQUIPOS(idEquipo)
-    on update cascade on delete cascade
-);
+-- -----------------------------------------------------
+-- Table `liga`.`equipo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `ligaACB`.`equipos` (
+  `idEquipo` INT(4) AUTO_INCREMENT PRIMARY KEY,
+  `nombreEquipo` VARCHAR(45) NOT NULL ,
+  `ciudad` VARCHAR(25) NOT NULL ,
+  `webOficial` VARCHAR(100),
+  `puntos` INT(3) NOT NULL);
 
-load data infile "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\equipos.csv"
-replace
-into table EQUIPOS
-FIELDS TERMINATED BY ','
+-- -----------------------------------------------------
+-- Table `liga`.`partido`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `ligaACB`.`partidos` (
+  `eLocal` INT(4) NOT NULL ,
+  `eVisitante` INT(4) NOT NULL ,
+  `resultado` VARCHAR(10) NOT NULL ,
+  `fecha` DATE NOT NULL ,
+  `arbitro` INT(3) NOT NULL ,
+
+  PRIMARY KEY (`eLocal`, `eVisitante`) ,
+
+  CONSTRAINT `fk_eLocal`
+    FOREIGN KEY (`eLocal`)
+    REFERENCES `ligaACB`.`equipos` (`idEquipo`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+  CONSTRAINT `fk_eVisitante`
+    FOREIGN KEY (`eVisitante`)
+    REFERENCES `ligaACB`.`equipos` (`idEquipo`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+
+
+-- -----------------------------------------------------
+-- Table `liga`.`jugador`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `ligaACB`.`jugadores` (
+  `idJugador` INT(4) AUTO_INCREMENT,
+  `nombre` VARCHAR(20) NOT NULL ,
+  `apellido` VARCHAR(20) NOT NULL ,
+  `puesto` SET('Escolta', 'Pivot', 'Alero','Base') NOT NULL ,
+  `idCapitan` INT(4) NOT NULL ,
+  `fechaAlta` DATE NOT NULL ,
+  `salario` DECIMAL(8,2) NOT NULL ,
+  `idEquipo` INT(4),
+  `altura` DECIMAL(3,2),
+
+  PRIMARY KEY (`idJugador`),
+
+  CONSTRAINT `fk_idEquipo`
+    FOREIGN KEY (`idEquipo` )
+    REFERENCES `ligaACB`.`equipos` (`idEquipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+
+  CONSTRAINT `fk_idCapitan`
+    FOREIGN KEY (`idCapitan` )
+    REFERENCES `ligaACB`.`jugadores` (`idJugador` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+-- CARGA DE DATOS
+LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\equipos.csv"
+REPLACE
+INTO TABLE equipos
+FIELDS TERMINATED BY ',' /*valor por defecto*/
+ENCLOSED BY '"'
 -- LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(idEquipo, nombreEquipo, ciudad, webOficial, puntos);
+IGNORE 1 LINES;
 
-select * from equipos;
+
+-- LOAD DATA LOCAL INFILE "C:\\Users\\wadmin\\Documents\\ligaACB\\jugadores.csv"
+LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\jugadores.csv"
+REPLACE
+INTO TABLE jugadores
+FIELDS TERMINATED BY ',' /*valor por defecto*/
+ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+
+
+-- LOAD DATA LOCAL INFILE "C:\\Users\\wadmin\\Documents\\ligaACB\\partidos.csv"
+LOAD DATA  INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\partidos.csv"
+REPLACE
+INTO TABLE partidos
+FIELDS TERMINATED BY ',' /*valor por defecto*/
+OPTIONALLY ENCLOSED BY '"'
+-- LINES TERMINATED BY '\r\n'
+ IGNORE 1 LINES;
---
+++ Bases de datos/Tema 4 - SQL/Scripts_SQL/SELECTS/selectLigaACB.sql
@@ -0,0 +1,49 @@
+# FILTROS-ORDER BY
+## 1.1
+### Mostrar todos los datos de los jugadores del equipo 3 ordenados por apellido
+select *
+from jugadores
+where idEquipo = 3
+order by apellido asc;
+
+## 1.2
+### Mostrar nombre, apellido e idJugador de los jugadores que juegan cómo ‘pivot ordenados por su identificador
+select nombre, apellido, idJugador -- , puesto
+from jugadores
+where puesto like 'pivot'
+order by idJugador;
+
+## 1.3
+### Mostrar nombre y apellido de los jugadores que midan más de 2 metros y ganen menos de 100000 euros
+select nombre, apellido -- , altura, salario
+from jugadores
+where altura > 2 and salario < 100000;
+
+## 1.4
+### Mostrar todos los datos de los partidos jugados en febrero
+select *
+from partidos
+where fecha like '%-02-%';
+
+## 1.5
+### Mostrar todo de los jugadores de los equipos 1 y 2 que ganen más de 80000 euros.
+select *
+from jugadores
+where salario > 80000 and
+	  idEquipo in (1,2);
+
+## 1.6
+### Mostrar todos los datos de los equipos cuya web termine en ‘.com’ o ‘.net’.
+select *
+from equipos
+where webOficial like '%.net'
+	  or webOficial like '%.com';
+
+
+
+
+
+
+
+
+
