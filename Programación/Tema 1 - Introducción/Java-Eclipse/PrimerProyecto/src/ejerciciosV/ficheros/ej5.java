e2bbeff0f653cacf3e08cb52a48c063e43b954bd
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-02-24 21:46:19 +0100

    Ejercicios archivos binarios

--- Programación/Tema 1 - Introducción/Java-Eclipse/PrimerProyecto/src/ejerciciosV/ficheros/ej5.java
+++ Programación/Tema 1 - Introducción/Java-Eclipse/PrimerProyecto/src/ejerciciosV/ficheros/ej5.java
@@ -4,27 +4,43 @@ import java.util.*;
 import java.io.*;
 
 public class ej5 {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static String ruta = "C:\\Users\\DAM1_Alu07\\Desktop\\DAM_git\\Programación\\Tema 1 - Introducción\\Java-Eclipse\\PrimerProyecto\\src\\ejerciciosV\\ficheros\\ej5.txt";
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void metodo1(){
 		double[] numeros = new double[10];
 		boolean numAsignado = false;
+		String string = "";
 		for (int i = 0; i < numeros.length; i++) {
 			do {
 				System.out.print("Introduzca un número real (posición " + i + "): ");
 				try {
 					numeros[i] = sc.nextDouble();
 					numAsignado = true;
+					if (i != 0)
+						string += "\n";
+					string += numeros[i];
 				} catch (InputMismatchException e) {
 					sc.nextLine();
 					numAsignado = false;
 					System.out.println("Número introducido no válido");
 				}
-				if(i > 0)
-				System.out.println(numeros[i-1]);
-				System.out.println(i);
 			} while (!numAsignado);
+
+		}
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(ruta))) {
+			bw.write(string);
+			System.out.println("Se han guardado 10 números reales en: " + ruta);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-
+	
+	public static void metodo2() {
+		
+	}
+	
+	public static void main(String[] args) {
+		metodo1();
+	}
 }
