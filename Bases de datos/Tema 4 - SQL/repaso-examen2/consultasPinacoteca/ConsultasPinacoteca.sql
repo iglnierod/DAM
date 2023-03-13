ae51ce65d96352894dc0e9f345d7204f2cd0588d
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-03-13 21:50:05 +0100

    bbdd

---
+++ Bases de datos/Tema 4 - SQL/repaso-examen2/consultasPinacoteca/ConsultasPinacoteca.sql
@@ -0,0 +1,39 @@
+-- Escribir Nombre y Apellidos
+
+-- 1._ Mostrar nombre de los pintores españoles y el nombre de su maestro, solo para aquellos que han tenido maestro.
+
+
+
+-- 2._ Crear una tabla llamada ‘PintoresEspañoles’ con los campos npintor, fnac, ffall. Establecer la clave primaria. 
+
+
+-- 3._ Añadir una columna a la tabla PintoresEspañoles, que se llame “Pais”, y que tenga por defecto el valor “España”.
+
+
+-- 4._ Volcar a la tabla PintoresEspañoles, los pintores españoles.
+
+
+-- 5._ Mostrar el nombre de los 5 cuadros más grandes, es decir, los que ocupan más. Ordenar por el nombre del cuadro.
+
+
+-- 6._ Mostrar para todos los cuadros, el nombre del cuadro, nombre del pintor, nombre de la pinacoteca donde se encuentra ubicado y 
+--     superficie del cuadro. No se mostrarán los cuadros de los que se desconozca la superficie. Ordenar por nombre de pintor. 
+
+
+-- 7._ Mostrar el nombre de cada pintor con el número total de sus cuadros sólo para aquellos pintores que tengan 2 o más obras pintadas con cualquier técnica basada en el óleo, ordenados por el número de cuadros.
+
+
+-- 8._ Mostrar el nombre de la escuela y el nombre del país al que pertenece dicha escuela para aquellos pintores que han utilizado 
+-- cualquier técnica basada en el óleo.
+
+                                  
+                                   
+-- 9._ Mostrar el nombre de los pintores que no tienen obras publicadas en las pinacotecas de Madrid. Ordenar por nombre del pintor.
+
+
+-- 10._Mostrar el nombre de los cuadros ubicados en la misma ciudad donde está la obra 'Galarina'.
+
+
+
+
+
