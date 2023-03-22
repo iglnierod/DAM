4b77f27a30c7cb38ea4fe38b821c9af9bab005a9
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-03-22 21:45:23 +0100

    Ej8 colecciones

--- Programación/Tema 1 - Introducción/Java-Eclipse/PrimerProyecto/src/colecciones/ejercicios/ej8.java
+++ Programación/Tema 1 - Introducción/Java-Eclipse/PrimerProyecto/src/colecciones/ejercicios/ej8.java
@@ -1,10 +1,28 @@
 package colecciones.ejercicios;
 
+import java.util.*;
+
 public class ej8 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void estadisticasPalabras() {
+		Scanner sc = new Scanner(System.in);
+		Set<String> palabras = new LinkedHashSet<>();
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Introduzca una palabra (" + (i+1) + "): ");
+			palabras.add(sc.nextLine());
+			Iterator<String> it = palabras.iterator();
+		}
+		
+		List<String> listOrd = new ArrayList<>(palabras);
+		Collections.sort(listOrd);
 
+		for (String string : listOrd) {
+			System.out.println(string + ": " + string.length());
+		}
+	}
+
+	public static void main(String[] args) {
+		estadisticasPalabras();
 	}
 
 }
