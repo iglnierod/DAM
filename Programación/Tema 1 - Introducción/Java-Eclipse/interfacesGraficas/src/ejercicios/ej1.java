cd43501eb4cffcb72844c608fe9a79cd80348d0b
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-04-12 21:41:59 +0200

    Ejercicios prog

---
+++ Programación/Tema 1 - Introducción/Java-Eclipse/interfacesGraficas/src/ejercicios/ej1.java
@@ -0,0 +1,60 @@
+package ejercicios;
+
+import java.awt.event.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.*;
+
+public class ej1 {
+	public static void main(String[] args) {
+		new Ventana();
+	}
+}
+
+class Ventana extends JFrame {
+	public Ventana() {
+		setTitle("Ej1");
+		setBounds(0, 0, 250, 300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		add(new Panel());
+
+		setVisible(true);
+	}
+}
+
+class Panel extends JPanel implements ActionListener {
+	JButton boton = new JButton("Mostrar hora");
+	JButton botonA = new JButton("A");
+	JButton botonB = new JButton("B");
+	JLabel label = new JLabel();
+
+	public Panel() {
+		botonA.addActionListener(this);
+		add(botonA);
+		
+		boton.addActionListener(this);
+		add(boton);
+		
+		botonB.addActionListener(this);
+		add(botonB);
+		
+		add(label);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "A": 
+			label.setText("Se ha pulsado el botón A");
+			break;
+		case "B":
+			label.setText("Se ha pulsado el botón B");
+			break;
+		case "Mostrar hora":
+			label.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS")));
+			break;
+		}
+	}
+}
