712596e80f924b20aad45df62c4a8cef0bc9980e
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-04-24 13:37:41 +0200

    prog

--- Programación/Tema 1 - Introducción/Java-Eclipse/interfacesGraficas/src/ejercicios/ej1.java
+++ Programación/Tema 1 - Introducción/Java-Eclipse/interfacesGraficas/src/ejercicios/ej1.java
@@ -1,68 +1,49 @@
 package ejercicios;
 
-import java.awt.event.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Constructor;
+
 import javax.swing.*;
 
 public class ej1 {
 	public static void main(String[] args) {
-		new Ventana();
+		new VentanaEj1();
 	}
 }
 
-class Ventana extends JFrame {
-	public Ventana() {
-		setTitle("Ej1");
-		setBounds(0, 0, 250, 300);
+class VentanaEj1 extends JFrame {
+
+	private JLabel lblSaludo;
+	private JTextField txfNombre;
+	private JButton btnSaludar;
+
+	public VentanaEj1() {
+		setTitle("Ejercicio 1");
+		setBounds(0, 0, 300, 150);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		add(new Panel());
-
-		setVisible(true);
-	}
-}
-
-class Panel extends JPanel implements ActionListener {
-	JButton boton = new JButton("Mostrar hora");
-	JButton botonA = new JButton("A");
-	JButton botonB = new JButton("B");
-	JTextField tfield = new JTextField("Hola que tal", 17);
-	JLabel label = new JLabel();
-
-	public Panel() {
-		botonA.addActionListener(this);
-		add(botonA);
-
-		boton.addActionListener(this);
-		add(boton);
-
-		botonB.addActionListener(this);
-		add(botonB);
-
-		tfield.addActionListener(this);
-		add(tfield);
-
-		add(label);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		switch (e.getActionCommand()) {
-		case "A":
-			label.setText("Se ha pulsado el botón A");
-			break;
-		case "B":
-			label.setText("Se ha pulsado el botón B");
-			break;
-		case "Mostrar hora":
-			label.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS")));
-			break;
-		}
-		Object source = e.getSource();
-		if (source == tfield)
-			label.setText("El texto es: " + tfield.getText());
+		JPanel pnlMain = new JPanel();
+		lblSaludo = new JLabel("Escribe un nobre para saludar");
+		txfNombre = new JTextField(10);
+		btnSaludar = new JButton("Saludar");
 		
+		btnSaludar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(VentanaEj1.this, "¡Hola " + txfNombre.getText().trim() + "!");
+			}
+		});
+		
+		pnlMain.add(lblSaludo);
+		pnlMain.add(txfNombre);
+		pnlMain.add(btnSaludar);
+		
+		getContentPane().add(pnlMain);
+		
+		setVisible(true);
 	}
 }
---
+++ Programación/Tema 1 - Introducción/Java-Eclipse/interfacesGraficas/src/ejercicios/ej2.java
@@ -0,0 +1,57 @@
+package ejercicios;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ej2 {
+	public static void main(String[] args) {
+		new VentanaEj2();
+	}
+}
+
+class VentanaEj2 extends JFrame {
+
+	private JPanel pnlIntroducir = new JPanel();
+	private JLabel lblTexto;
+	private JTextField txfMarca = new JTextField(10);
+	private JButton btnAnadir = new JButton("Añadir");
+
+	private JPanel pnlMarcas = new JPanel(new BorderLayout());
+	private JComboBox<String> cmbMarcas = new JComboBox<String>();
+
+	public VentanaEj2() {
+		setTitle("Ejercicio 1");
+		setBounds(0, 0, 500, 100);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new GridLayout(1, 2));
+		
+		cmbMarcas.setBounds(0, 0, 30, 10);
+		
+		addToPnl(pnlIntroducir, lblTexto = new JLabel("Introduzca una marca de coches"));
+		addToPnl(pnlIntroducir, txfMarca);
+		addToPnl(pnlIntroducir, btnAnadir);
+
+		JPanel pnlNorth = new JPanel();
+		pnlNorth.add(lblTexto = new JLabel("MARCAS:"));
+		pnlNorth.add(cmbMarcas);
+		
+		pnlMarcas.add(pnlNorth, BorderLayout.NORTH);
+		
+		
+		getContentPane().add(pnlIntroducir);
+		getContentPane().add(pnlMarcas);
+
+		
+		setVisible(true);
+	}
+
+	private void addToPnl(JPanel pnl, JComponent o) {
+		pnl.add(o);
+	}
+
+	private void anadirMarca(String s) {
+		if (!s.equals(" "))
+			cmbMarcas.addItem(s.trim());
+	}
+}
