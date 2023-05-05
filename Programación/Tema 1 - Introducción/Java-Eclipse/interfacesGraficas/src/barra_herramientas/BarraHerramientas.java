1b22de4ce4b5f3d7b311632fe7fa6c6c25df6cbd
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-05-05 18:08:12 +0200

    progr

--- Programación/Tema 1 - Introducción/Java-Eclipse/interfacesGraficas/src/barra_herramientas/BarraHerramientas.java
+++ Programación/Tema 1 - Introducción/Java-Eclipse/interfacesGraficas/src/barra_herramientas/BarraHerramientas.java
@@ -17,15 +17,23 @@ class VentanaBarraHerramientas extends JFrame {
 		setBounds(1400, 300, 500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		AccionColor accionRojo = new AccionColor("rojo", Color.RED, new ImageIcon("recursos/hex16rojo.png"));
-		AccionColor accionVerde = new AccionColor("verde", Color.GREEN, new ImageIcon("recursos/hex16verde.png"));
-		AccionColor accionAzul = new AccionColor("azul", Color.BLUE, new ImageIcon("recursos/hex16azul.png"));
-
+		AccionColor accionRojo = new AccionColor("rojo", Color.RED, new ImageIcon("recursos/rojo.png"));
+		AccionColor accionVerde = new AccionColor("verde", Color.GREEN, new ImageIcon("recursos/verde.png"));
+		AccionColor accionAzul = new AccionColor("azul", Color.BLUE, new ImageIcon("recursos/azul.png"));
 		JToolBar tlb = new JToolBar("Colores");
 		tlb.add(accionRojo);
 		tlb.add(accionVerde);
 		tlb.add(accionAzul);
 
+		AbstractAction accionCerrar = new AbstractAction("cerrar",new ImageIcon("recursos/cancel.png")) {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		};
+		tlb.addSeparator();
+		tlb.add(accionCerrar);
 		getContentPane().add(tlb, BorderLayout.NORTH);
 
 		setVisible(true);
