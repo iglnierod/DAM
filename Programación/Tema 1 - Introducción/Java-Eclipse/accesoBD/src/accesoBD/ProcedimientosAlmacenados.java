0a5e3572baaaa1a29c9244968aaa4084936c3833
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-05-24 21:41:44 +0200

    + procedimientos

--- Programación/Tema 1 - Introducción/Java-Eclipse/accesoBD/src/accesoBD/ProcedimientosAlmacenados.java
+++ Programación/Tema 1 - Introducción/Java-Eclipse/accesoBD/src/accesoBD/ProcedimientosAlmacenados.java
@@ -10,12 +10,14 @@ public class ProcedimientosAlmacenados {
 	public static void main(String[] args) {
 		mostrarEmailsEmpleadosEMEA();
 		mostrarEmailEmpleadosDeTerritorio("EMEA");
+		mostrarUltimosPagos();
 	}
 
 	private static void mostrarEmailsEmpleadosEMEA() {
 		try (Connection con = DriverManager.getConnection(URL, USUARIO, CLAVE);
 				CallableStatement cs = con.prepareCall("{call ObtenerEmailEmpleadosEMEA}")) {
 			try (ResultSet rs = cs.executeQuery()) {
+				System.out.println("--------- ObtenerEmailEmpleadosEMEA ---------");
 				while (rs.next()) {
 					System.out.println(rs.getString(1));
 				}
@@ -27,11 +29,34 @@ public class ProcedimientosAlmacenados {
 
 	private static void mostrarEmailEmpleadosDeTerritorio(String territorio) {
 		try (Connection con = DriverManager.getConnection(URL, USUARIO, CLAVE);
-				CallableStatement cs = con.prepareCall("{call ObtenerEmailEmpleadosDeTerritorio(" + territorio + ");}")) {
+				CallableStatement cs = con.prepareCall("{call ObtenerEmailEmpleadosDeTerritorio(?)};")) {
+			cs.setString(1, territorio);
 			try (ResultSet rs = cs.executeQuery()) {
+				System.out.println("--------- mostrarEmailEmpleadosDeTerritorio ---------");
 				while (rs.next()) {
-					System.out.println(rs.getString(1));
+					System.out.println(
+							rs.getString("Nombre") + " " + rs.getString("Apellidos") + " " + rs.getString("email"));
 				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void mostrarUltimosPagos() {
+		try (Connection con = DriverManager.getConnection(URL, USUARIO, CLAVE);
+				CallableStatement cs = con.prepareCall("{call ObtenerUltimosPagos};")) {
+			try (ResultSet rs = cs.executeQuery()) {
+				System.out.println("ULTIMOS 10 PAGOS:");
+				double total = 0;
+				while (rs.next()) {
+					String fecha = String.format("[%1$tA %1$td de %1$tB de %1$tY]", rs.getDate(3));
+					System.out.printf("(%s) %11s %32s: %10.2f\n", rs.getString(1), rs.getString(2), fecha,
+							rs.getDouble(4));
+					total += rs.getDouble(4);
+				}
+				System.out.println("-".repeat(62));
+				System.out.printf("%51s %10.2f", "TOTAL:", total);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
